a2f718c96600f08bb8d18228b4ac526fc6136e22
Kostya <betenya.kostya@mail.ru>
2024-03-22 22:03:42 +0300

    free form create

---
+++ TaskControlManager.dpr
@@ -0,0 +1,14 @@
+program TaskControlManager;
+
+uses
+  Vcl.Forms,
+  TasksListScreenUnit in 'TasksListScreenUnit.pas' {Form5};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm5, Form5);
+  Application.Run;
+end.
