57fe4e92152a5e2ce286bc8f95890bd678f2ee1f
Kostya <betenya.kostya@mail.ru>
2024-03-30 23:37:10 +0300

    Main Screen modified

--- TasksListScreenUnit.pas
+++ TasksListScreenUnit.pas
@@ -21,93 +21,199 @@ Uses
     Vcl.VirtualImage,
     Vcl.BaseImageCollection,
     Vcl.ImageCollection,
-    Vcl.VirtualImageList;
+    Vcl.VirtualImageList,
+    ButtonFrame,
+    TasksLinkedListUnit,
+    TaskFrame,
+    TaskUnit,
+    UserUnit;
 
 Type
     TTaskListForm = Class(TForm)
         PaintBox2: TPaintBox;
-        PaintBox4: TPaintBox;
+        TasksListPaintBox: TPaintBox;
         MenuBackGround: TPaintBox;
-        ImageCollection: TImageCollection;
-        AddTask: TVirtualImage;
-        Store: TVirtualImage;
-        Profile: TVirtualImage;
-        Refferencess: TVirtualImage;
-        Settings: TVirtualImage;
         Image1: TImage;
         Image2: TImage;
-        Label1: TLabel;
+        CoinsLabel: TLabel;
         Label2: TLabel;
-        Label3: TLabel;
+        LvlLabel: TLabel;
         Label4: TLabel;
-        Label5: TLabel;
-        Label6: TLabel;
+        XPLabel: TLabel;
+        HPLabel: TLabel;
+        XPBox: TPaintBox;
         HPPBox: TPaintBox;
-        EXPPBox: TPaintBox;
-        PaintBox1: TPaintBox;
+        Frame11: TFrame1;
+        Frame12: TFrame1;
+        Frame13: TFrame1;
+        Frame14: TFrame1;
+        AddTaskFrame: TFrame1;
+        TasksListSclBox: TScrollBox;
+        ImageCollection: TImageCollection;
         Procedure FormCreate(Sender: TObject);
         Procedure PaintBox2Paint(Sender: TObject);
-        Procedure PaintBox4Paint(Sender: TObject);
+        Procedure TasksListPaintBoxPaint(Sender: TObject);
         Procedure MenuBackGroundPaint(Sender: TObject);
-        Procedure AddTaskMouseLeave(Sender: TObject);
-        Procedure AddTaskMouseEnter(Sender: TObject);
-        Procedure StoreMouseEnter(Sender: TObject);
-        Procedure StoreMouseLeave(Sender: TObject);
-        Procedure ProfileMouseLeave(Sender: TObject);
-        Procedure ProfileMouseEnter(Sender: TObject);
-        Procedure RefferencessMouseLeave(Sender: TObject);
-        Procedure RefferencessMouseEnter(Sender: TObject);
-        Procedure SettingsMouseEnter(Sender: TObject);
-        Procedure SettingsMouseLeave(Sender: TObject);
-        Procedure ExpPBoxPaint(Sender: TObject);
         Procedure HPPBoxPaint(Sender: TObject);
-        Procedure AddTaskClick(Sender: TObject);
-    procedure StoreClick(Sender: TObject);
-    procedure SettingsClick(Sender: TObject);
+        Procedure XPBoxPaint(Sender: TObject);
+        Procedure Frame11VirtualImage1Click(Sender: TObject);
+        Procedure Frame11Label1Click(Sender: TObject);
+        Procedure Frame13Label1Click(Sender: TObject);
+        Procedure Frame13VirtualImage1Click(Sender: TObject);
+        Procedure Frame15VirtualImage1Click(Sender: TObject);
+        Procedure Frame14Label1Click(Sender: TObject);
+        Procedure Frame14VirtualImage1Click(Sender: TObject);
+        Procedure Frame12VirtualImage1Click(Sender: TObject);
+        Procedure Frame12Label1Click(Sender: TObject);
+        Procedure AddNewBlockInBlocksArr;
+        Procedure TasksListSclBoxMouseWheel(Sender: TObject; Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint;
+            Var Handled: Boolean);
+        Procedure UpDateUserInfo();
     Private
         { Private declarations }
     Public
         { Public declarations }
     End;
 
+Type
+    TArrayOfBlocks = Array Of TTaskOutputFrame;
+    TChoosenOpenButton = (Info, Nothing);
+
 Var
     TaskListForm: TTaskListForm;
+    TasksList: TListOfTasks;
+    ChoosenOpenButton: TChoosenOpenButton = Nothing;
+    FArrayOfBlocks: TArrayOfBlocks;
+    User: TUser;
 
 Implementation
 
 {$R *.dfm}
 
 Uses
-    NewTaskUnit, StoreUnit, SettingsUnit;
+    NewTaskUnit,
+    StoreUnit,
+    SettingsUnit,
+    InstractionUnit,
+    ProfileUnit;
+
+Procedure TTaskListForm.UpDateUserInfo();
+Begin
+    XPBoxPaint(TaskListForm.XPBox);
+    XPBox.Hint := IntToStr(User.XP) + '/' + IntToStr(User.MaxXP);
+    HPPBoxPaint(TaskListForm.HPPBox); 
+    HPPBox.Hint := IntToStr(User.HP) + '/' + IntToStr(User.MaxHP);
+    LvlLabel.Caption := IntToStr(User.CurentLvl);
+    CoinsLabel.Caption := IntToStr(User.Coins);
+End;
+
+Procedure TTaskListForm.AddNewBlockInBlocksArr;
+Var
+    TempArrOfBlocks: TArrayOfBlocks;
+    I, Position: Integer;
+Begin
+    Position := TasksListSclBox.VertScrollBar.Position;
+    TasksListSclBox.VertScrollBar.Position := 0;
+    SetLength(TempArrOfBlocks, TasksList.FTasksCounter);
+    For I := Low(FArrayOfBlocks) To High(FArrayOfBlocks) Do
+        TempArrOfBlocks[I] := FArrayOfBlocks[I];
+    TempArrOfBlocks[High(TempArrOfBlocks)] := TTaskOutputFrame.Create(TasksListPaintBox);
+    TempArrOfBlocks[High(TempArrOfBlocks)].Name := 'Task' + IntToStr(High(TempArrOfBlocks));
+    TempArrOfBlocks[High(TempArrOfBlocks)].Parent := TasksListSclBox;
+    TasksList.InputInfoInTask(TempArrOfBlocks[High(TempArrOfBlocks)], High(TempArrOfBlocks));
+    FArrayOfBlocks := TempArrOfBlocks;
+    TasksListPaintBoxPaint(TasksListPaintBox);
+    TasksListSclBox.VertScrollBar.Position := Position;
+End;
 
 Procedure TTaskListForm.FormCreate(Sender: TObject);
 Begin
+    TasksList := TListOfTasks.Create;
+    User := TUser.Create;
+    UpDateUserInfo;
     TaskListForm.Color := RGB(202, 205, 221);
 End;
 
-Procedure TTaskListForm.HPPBoxPaint(Sender: TObject);
+Procedure TTaskListForm.Frame11Label1Click(Sender: TObject);
+Begin
+    StoreForm.ShowModal;
+End;
+
+Procedure TTaskListForm.Frame11VirtualImage1Click(Sender: TObject);
+Begin
+    StoreForm.ShowModal;
+End;
+
+Procedure TTaskListForm.Frame12Label1Click(Sender: TObject);
+Begin
+    ProfileForm.ShowModal;
+End;
+
+Procedure TTaskListForm.Frame12VirtualImage1Click(Sender: TObject);
+Begin
+    ProfileForm.ShowModal;
+End;
+
+Procedure TTaskListForm.Frame13Label1Click(Sender: TObject);
+Begin
+    Form1.ShowModal;
+End;
+
+Procedure TTaskListForm.Frame13VirtualImage1Click(Sender: TObject);
+Begin
+    Form1.ShowModal;
+End;
+
+Procedure TTaskListForm.Frame14Label1Click(Sender: TObject);
+Begin
+    InstractionForm.ShowModal;
+End;
+
+Procedure TTaskListForm.Frame14VirtualImage1Click(Sender: TObject);
+Begin
+    InstractionForm.ShowModal;
+End;
+
+Procedure TTaskListForm.Frame15VirtualImage1Click(Sender: TObject);
+Var
+    Complexity: TComplexity;
+Begin
+    NewTask := TTask.Create;
+    Application.CreateForm(TNewTaskForm, NewTaskForm);
+    NewTaskForm.ShowModal;
+    If (ChoosenOpenButton = Info) Then
+    Begin
+        Complexity := NewTask.ComplexityDeterminant(NewTaskForm.ComplexityCBox.ItemIndex);
+        NewTask.InputMainData(NewTaskForm.TitleLEdit.Text, NewTaskForm.DateTPicker.Date, NewTaskForm.AboutTaskMemo.Text, Complexity);
+        TasksList.AddTaskInList(NewTask);
+        TaskListForm.AddNewBlockInBlocksArr;
+        ChoosenOpenButton := Nothing;
+    End;
+End;
+
+Procedure TTaskListForm.XPBoxPaint(Sender: TObject);
 Var
     BitMap: TBitmap;
     Rect: TRect;
     Radius: Integer;
 Begin
     BitMap := TBitmap.Create();
-    BitMap.Height := HPPBox.Height;
-    BitMap.Width := HPPBox.Width;
+    BitMap.Height := XPBox.Height;
+    BitMap.Width := XPBox.Width;
 
-    HPPBox.Canvas.Brush.Color := RGB(179, 186, 200);
-    HPPBox.Canvas.Pen.Color := RGB(179, 186, 200);
-    HPPBox.Canvas.Pen.Width := 2;
+    XPBox.Canvas.Brush.Color := RGB(179, 186, 200);
+    XPBox.Canvas.Pen.Color := RGB(179, 186, 200);
+    XPBox.Canvas.Pen.Width := 2;
     Radius := 10;
-    Rect := HPPBox.ClientRect;
-    HPPBox.Canvas.RoundRect(Rect.Left, Rect.Top, Rect.Right, Rect.Bottom, Radius, Radius);
+    Rect := XPBox.ClientRect;
+    XPBox.Canvas.RoundRect(Rect.Left, Rect.Top, Rect.Right, Rect.Bottom, Radius, Radius);
 
-    HPPBox.Canvas.Brush.Color := RGB(26, 155, 85);
-    HPPBox.Canvas.Pen.Color := RGB(26, 155, 85);
-    HPPBox.Canvas.Pen.Width := 2;
+    XPBox.Canvas.Brush.Color := RGB(26, 155, 85);
+    XPBox.Canvas.Pen.Color := RGB(26, 155, 85);
+    XPBox.Canvas.Pen.Width := 2;
     Radius := 10;
-    Rect := HPPBox.ClientRect;
-    HPPBox.Canvas.RoundRect(Rect.Left, Rect.Top, Rect.Right Div 2, Rect.Bottom, Radius, Radius);
+    Rect := XPBox.ClientRect;
+    XPBox.Canvas.RoundRect(Rect.Left, Rect.Top, Rect.Right * User.GetXpPersent Div 100, Rect.Bottom, Radius, Radius);
 
     BitMap.Free();
 End;
@@ -127,8 +233,6 @@ Begin
     PaintBox2.Canvas.Pen.Color := RGB(179, 186, 200);
     PaintBox2.Canvas.Pen.Width := 2;
     PaintBox2.Canvas.RoundRect(Rect.Left, Rect.Top, Rect.Right, Rect.Bottom, Radius, Radius);
-
-    //TaskListForm.Canvas.Draw(PaintBox2.Left, PaintBox2.Top, BitMap);
     BitMap.Free();
 End;
 
@@ -151,112 +255,61 @@ Begin
     BitMap.Free();
 End;
 
-Procedure TTaskListForm.PaintBox4Paint(Sender: TObject);
+Procedure TTaskListForm.TasksListPaintBoxPaint(Sender: TObject);
 Var
     BitMap: TBitmap;
     Rect: TRect;
     Radius: Integer;
 Begin
+    If (TasksList.FTasksCounter > 6) Then
+        TasksListPaintBox.Height := 8 + TasksList.FTasksCounter * (8 + FArrayOfBlocks[High(FArrayOfBlocks)].Height);
+
     BitMap := TBitmap.Create();
-    Rect := PaintBox4.ClientRect;
-    BitMap.Height := PaintBox4.Height;
-    BitMap.Width := PaintBox4.Width;
+    Rect := TasksListPaintBox.ClientRect;
+    BitMap.Height := TasksListPaintBox.Height;
+    BitMap.Width := TasksListPaintBox.Width;
     Radius := 30;
-    PaintBox4.Canvas.Brush.Color := Rgb(130, 138, 157);
-    PaintBox4.Canvas.Pen.Color := Rgb(179, 186, 200);
-    PaintBox4.Canvas.Pen.Width := 10;
-    PaintBox4.Canvas.RoundRect(Rect.Left, Rect.Top, Rect.Right, Rect.Bottom, Radius, Radius);
+    TasksListPaintBox.Canvas.Brush.Color := Rgb(130, 138, 157);
+    TasksListPaintBox.Canvas.Pen.Color := Rgb(179, 186, 200);
+    TasksListPaintBox.Canvas.Pen.Width := 10;
+    TasksListPaintBox.Canvas.RoundRect(Rect.Left, Rect.Top, Rect.Right, Rect.Bottom, Radius, Radius);
     BitMap.Free();
 End;
 
-Procedure TTaskListForm.AddTaskClick(Sender: TObject);
+Procedure TTaskListForm.TasksListSclBoxMouseWheel(Sender: TObject; Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint;
+    Var Handled: Boolean);
+Const
+    ScrollSpeed = 10;
+Var
+    Delta: Integer;
 Begin
-    Application.CreateForm(TNewTaskForm, NewTaskForm);
-    NewTaskForm.ShowModal;
+    Delta := WheelDelta Div 60 * ScrollSpeed;
+    TasksListSclBox.VertScrollBar.Position := TasksListSclBox.VertScrollBar.Position - Delta;
+    Handled := True;
 End;
 
-Procedure TTaskListForm.AddTaskMouseEnter(Sender: TObject);
-Begin
-    AddTask.ImageIndex := 1;
-End;
-
-Procedure TTaskListForm.AddTaskMouseLeave(Sender: TObject);
-Begin
-    AddTask.ImageIndex := 0;
-End;
-
-Procedure TTaskListForm.ExpPBoxPaint(Sender: TObject);
+Procedure TTaskListForm.HPPBoxPaint(Sender: TObject);
 Var
     BitMap: TBitmap;
     Rect: TRect;
     Radius: Integer;
 Begin
     BitMap := TBitmap.Create();
-    BitMap.Height := ExpPBox.Height;
-    BitMap.Width := ExpPBox.Width;
+    BitMap.Height := HPPBox.Height;
+    BitMap.Width := HPPBox.Width;
 
-    ExpPBox.Canvas.Brush.Color := RGB(179, 186, 200);
-    ExpPBox.Canvas.Pen.Color := RGB(179, 186, 200);
-    ExpPBox.Canvas.Pen.Width := 2;
+    HPPBox.Canvas.Brush.Color := RGB(179, 186, 200);
+    HPPBox.Canvas.Pen.Color := RGB(179, 186, 200);
+    HPPBox.Canvas.Pen.Width := 2;
     Radius := 10;
-    Rect := ExpPBox.ClientRect;
-    ExpPBox.Canvas.RoundRect(Rect.Left, Rect.Top, Rect.Right, Rect.Bottom, Radius, Radius);
+    Rect := HPPBox.ClientRect;
+    HPPBox.Canvas.RoundRect(Rect.Left, Rect.Top, Rect.Right, Rect.Bottom, Radius, Radius);
 
-    ExpPBox.Canvas.Brush.Color := RGB(186, 55, 71);
-    ExpPBox.Canvas.Pen.Color := RGB(186, 55, 71);
-    ExpPBox.Canvas.RoundRect(Rect.Left, Rect.Top, Rect.Right Div 2, Rect.Bottom, Radius, Radius);
+    HPPBox.Canvas.Brush.Color := RGB(186, 55, 71);
+    HPPBox.Canvas.Pen.Color := RGB(186, 55, 71);
+    HPPBox.Canvas.RoundRect(Rect.Left, Rect.Top, Rect.Right * User.GetHpPersent Div 100, Rect.Bottom, Radius, Radius);
 
     BitMap.Free();
-End;
-
-procedure TTaskListForm.StoreClick(Sender: TObject);
-begin
-    StoreForm.ShowModal;
-end;
-
-Procedure TTaskListForm.StoreMouseEnter(Sender: TObject);
-Begin
-    Store.ImageIndex := 3;
-End;
-
-Procedure TTaskListForm.StoreMouseLeave(Sender: TObject);
-Begin
-    Store.ImageIndex := 2;
-End;
-
-Procedure TTaskListForm.ProfileMouseEnter(Sender: TObject);
-Begin
-    Profile.ImageIndex := 5;
-End;
-
-Procedure TTaskListForm.ProfileMouseLeave(Sender: TObject);
-Begin
-    Profile.ImageIndex := 4;
-End;
-
-Procedure TTaskListForm.RefferencessMouseEnter(Sender: TObject);
-Begin
-    Refferencess.ImageIndex := 7;
-End;
-
-Procedure TTaskListForm.RefferencessMouseLeave(Sender: TObject);
-Begin
-    Refferencess.ImageIndex := 6;
-End;
-
-procedure TTaskListForm.SettingsClick(Sender: TObject);
-begin
-    Form1.ShowModal;
-end;
-
-Procedure TTaskListForm.SettingsMouseEnter(Sender: TObject);
-Begin
-    Settings.ImageIndex := 9;
-End;
-
-Procedure TTaskListForm.SettingsMouseLeave(Sender: TObject);
-Begin
-    Settings.ImageIndex := 8;
 End;
 
 End.
