1978819d0f5aa46d9d6d54a994e14fdb34bec3ab
Kostya <betenya.kostya@mail.ru>
2024-03-30 23:33:08 +0300

    SubTasksOutput

---
+++ ViewSubTasksUnit.dfm
@@ -0,0 +1,48 @@
+object SubTasksForm: TSubTasksForm
+  Left = 0
+  Top = 0
+  BorderIcons = [biSystemMenu]
+  BorderStyle = bsSingle
+  Caption = #1055#1086#1076#1079#1072#1076#1072#1095#1080
+  ClientHeight = 312
+  ClientWidth = 535
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  Position = poScreenCenter
+  OnCreate = FormCreate
+  TextHeight = 15
+  object MainLabel: TLabel
+    Left = 174
+    Top = 10
+    Width = 186
+    Height = 25
+    Caption = #1057#1055#1048#1057#1054#1050' '#1055#1054#1047#1044#1040#1044#1040#1063
+    Font.Charset = DEFAULT_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -18
+    Font.Name = 'Segoe UI'
+    Font.Style = [fsBold]
+    ParentFont = False
+  end
+  object SubTasksSclBox: TScrollBox
+    Left = 10
+    Top = 48
+    Width = 510
+    Height = 249
+    HorzScrollBar.Visible = False
+    VertScrollBar.Tracking = True
+    TabOrder = 0
+    OnMouseWheel = SubTasksSclBoxMouseWheel
+    object SubTasksPBox: TPaintBox
+      Left = 0
+      Top = 0
+      Width = 489
+      Height = 246
+      OnPaint = SubTasksPBoxPaint
+    end
+  end
+end
---
+++ ViewSubTasksUnit.pas
@@ -0,0 +1,102 @@
+﻿Unit ViewSubTasksUnit;
+
+Interface
+
+Uses
+    Winapi.Windows,
+    Winapi.Messages,
+    System.SysUtils,
+    System.Variants,
+    System.Classes,
+    Vcl.Graphics,
+    Vcl.Controls,
+    Vcl.Forms,
+    Vcl.Dialogs,
+    Vcl.StdCtrls,
+    Vcl.ExtCtrls,
+    TasksLinkedListUnit,
+    TaskUnit;
+
+Type
+    TSubTasksForm = Class(TForm)
+        MainLabel: TLabel;
+        SubTasksSclBox: TScrollBox;
+        SubTasksPBox: TPaintBox;
+        Procedure FormCreate(Sender: TObject);
+        Procedure SubTasksPBoxPaint(Sender: TObject);
+        Procedure SubTasksSclBoxMouseWheel(Sender: TObject; Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint;
+            Var Handled: Boolean);
+    Private
+        { Private declarations }
+    Public
+        { Public declarations }
+    End;
+
+Const
+    LeftBorder: Integer = 25;
+    TopBorder: Integer = 25;
+    WidthBorder: Integer = 612;
+    HeightBorder: Integer = 50;
+
+Var
+    SubTasksForm: TSubTasksForm;
+    ArrayOfTasks: TArrayOfTasks;
+
+Implementation
+
+{$R *.dfm}
+
+Uses
+    TasksListScreenUnit,
+    ViewControllerUnit,
+    NewTaskUnit,
+    ButtonFrame,
+    ChangeDataUnit;
+
+Procedure TSubTasksForm.FormCreate(Sender: TObject);
+Begin
+    MainLabel.Font.Color := Rgb(38, 43, 50);
+    SubTasksForm.Color := Rgb(202, 205, 221);
+    SubTasksSclBox.VertScrollBar.Color := Rgb(202, 205, 221);
+    NewTask.OutputText(LeftBorder, TopBorder, WidthBorder, HeightBorder);
+End;
+
+Procedure TSubTasksForm.SubTasksPBoxPaint(Sender: TObject);
+Var
+    BitMap: TBitmap;
+    RectM: TRect;
+    Radius: Integer;
+Begin
+    BitMap := TBitmap.Create();
+
+    If NewTask.SubTasksCounter > 3 Then
+        SubTasksPBox.Height := NewTask.SubTasksCounter * (TopBorder + HeightBorder) + 20;
+
+    RectM := SubTasksPBox.ClientRect;
+    BitMap.Height := SubTasksPBox.Height;
+    BitMap.Width := SubTasksPBox.Width;
+    Radius := 30;
+    BitMap.Canvas.Brush.Color := Rgb(169, 174, 187);
+    BitMap.Canvas.Pen.Color := Rgb(179, 188, 206);
+    BitMap.Canvas.Pen.Width := 10;
+    BitMap.Canvas.RoundRect(RectM.Left, RectM.Top, RectM.Right, RectM.Bottom, Radius, Radius);
+
+    NewTask.DrawSubTasks(BitMap.Canvas, SubTasksPBox, LeftBorder, TopBorder, WidthBorder, HeightBorder);
+
+    SubTasksPBox.Canvas.Draw(0, 0, Bitmap);
+    BitMap.Free();
+End;
+
+Procedure TSubTasksForm.SubTasksSclBoxMouseWheel(Sender: TObject; Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint;
+    Var Handled: Boolean);
+Const
+    ScrollSpeed = 10;
+Var
+    Delta: Integer;
+Begin
+    Delta := WheelDelta Div 60 * ScrollSpeed;
+    SubTasksSclBox.VertScrollBar.Position := SubTasksSclBox.VertScrollBar.Position - Delta;
+    Handled := True;
+End;
+
+End.
