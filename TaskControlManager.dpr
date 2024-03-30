fe34dd21a7acb5788c5799290e57c0399a8f814c
Kostya <betenya.kostya@mail.ru>
2024-03-30 23:37:28 +0300

    MAIN DPROJ MODIFIED

--- TaskControlManager.dpr
+++ TaskControlManager.dpr
@@ -8,7 +8,16 @@ uses
   Vcl.Styles,
   StoreUnit in 'StoreUnit.pas' {StoreForm},
   ButtonFrame in 'ButtonFrame.pas' {Frame1: TFrame},
-  SettingsUnit in 'SettingsUnit.pas' {Form1};
+  SettingsUnit in 'SettingsUnit.pas' {Form1},
+  TaskUnit in 'TaskUnit.pas',
+  ViewControllerUnit in 'ViewControllerUnit.pas',
+  InstractionUnit in 'InstractionUnit.pas' {InstractionForm},
+  ProfileUnit in 'ProfileUnit.pas' {ProfileForm},
+  TasksLinkedListUnit in 'TasksLinkedListUnit.pas',
+  ViewSubTasksUnit in 'ViewSubTasksUnit.pas' {SubTasksForm},
+  ChangeDataUnit in 'ChangeDataUnit.pas' {ChangeDataForm},
+  TaskFrame in 'TaskFrame.pas' {TaskOutputFrame: TFrame},
+  UserUnit in 'UserUnit.pas';
 
 {$R *.res}
 
@@ -18,6 +27,8 @@ Begin
     Application.CreateForm(TTaskListForm, TaskListForm);
   Application.CreateForm(TStoreForm, StoreForm);
   Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TInstractionForm, InstractionForm);
+  Application.CreateForm(TProfileForm, ProfileForm);
   Application.Run;
 
 End.
