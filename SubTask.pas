e223875c5a6395fd7c72ad558fd61f2d212b391a
Kostya <betenya.kostya@mail.ru>
2024-03-30 23:32:45 +0300

    clearSubTask

---
+++ SubTask.pas
@@ -0,0 +1,32 @@
+unit SubTask;
+
+interface
+
+Uses
+    Winapi.Windows,
+    Winapi.Messages,
+    System.SysUtils,
+    System.Variants,
+    System.Classes,
+    Vcl.Graphics,
+    Vcl.Controls,
+    Vcl.Forms,
+    Vcl.Dialogs,
+    Vcl.ExtCtrls,
+    Vcl.StdCtrls,
+    Vcl.Mask,
+    Vcl.ComCtrls,
+    Vcl.VirtualImage,
+    Vcl.BaseImageCollection,
+    Vcl.ImageCollection,
+    System.ImageList,
+    Vcl.ImgList,
+    Vcl.VirtualImageList,
+    ButtonFrame;
+
+Type
+    TSubTask
+
+implementation
+
+end.
