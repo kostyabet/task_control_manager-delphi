d3361ab25436e37f6448283266acee9649309ef5
Kostya <betenya.kostya@mail.ru>
2024-03-30 23:34:46 +0300

    Clear Instraction form create

---
+++ InstractionUnit.pas
@@ -0,0 +1,31 @@
+Unit InstractionUnit;
+
+Interface
+
+Uses
+    Winapi.Windows,
+    Winapi.Messages,
+    System.SysUtils,
+    System.Variants,
+    System.Classes,
+    Vcl.Graphics,
+    Vcl.Controls,
+    Vcl.Forms,
+    Vcl.Dialogs;
+
+Type
+    TInstractionForm = Class(TForm)
+    Private
+        { Private declarations }
+    Public
+        { Public declarations }
+    End;
+
+Var
+    InstractionForm: TInstractionForm;
+
+Implementation
+
+{$R *.dfm}
+
+End.
