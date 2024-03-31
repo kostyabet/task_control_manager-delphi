1bb902bfa8e7268bb54f15ea647849c2f46ad936
Kostya <betenya.kostya@mail.ru>
2024-03-31 23:59:52 +0300

    User modified

--- UserUnit.pas
+++ UserUnit.pas
@@ -9,6 +9,25 @@ Interface
 //- кол-во монет.
 
 Uses
+    Winapi.Windows,
+    Winapi.Messages,
+    System.SysUtils,
+    System.Variants,
+    System.Classes,
+    Vcl.Graphics,
+    Vcl.Controls,
+    Vcl.Forms,
+    Vcl.Dialogs,
+    Vcl.ExtCtrls,
+    Vcl.Buttons,
+    System.ImageList,
+    Vcl.ImgList,
+    Vcl.StdCtrls,
+    Vcl.Imaging.Pngimage,
+    Vcl.VirtualImage,
+    Vcl.BaseImageCollection,
+    Vcl.ImageCollection,
+    Vcl.VirtualImageList,
     TaskUnit;
 
 Type
@@ -16,7 +35,7 @@ Type
 
     TUser = Class
     Const
-        StartXP: Integer = 100;
+        StartXP: Integer = 50;
         StartHP: Integer = 25;
 
         EazyTaskXP: Integer = 10;
@@ -35,6 +54,21 @@ Type
         AwardIncrease: Double = 0.1;
         XPIncrease: Double = 1.1;
         HPIncrease: Double = 1.05;
+        CostsIncrease: Double = 1.1;
+
+        HPCost: Integer = 50;
+        XPCost: Integer = 75;
+        HPBustCost: Integer = 20;
+        XPBustCost: Integer = 30;
+        CoinsBustCost: Integer = 20;
+        TothemBustCost: Integer = 100;
+        FreeTaskBust: Integer = 80;
+        SecretBox: Integer = 70;
+
+    Type
+        TBusts = (HP, XP, HPBust, XPBust, CoinsBust, Tothem, FreeTask, SecretBox);
+        TBustsCost = Array [Low(TBusts) .. High(TBusts)] Of Integer;
+        TBustBought = Array [Low(TBusts) .. TBusts.FreeTask] Of Integer;
 
     Var
         FCurentLvl: Integer;
@@ -43,8 +77,13 @@ Type
         FHP: Integer;
         FNextLvlHP: Integer;
         FCoins: Integer;
+        FBustsCost: TBustsCost;
+        FBustsBuyCount: Integer;
+        FBustBought: TBustBought;
     Private
         Function CalculateAwards(Complexity: TComplexity; EasyVal, MediumVal, HardVal: Integer): Integer;
+
+        Function FindCost(StartVal: Integer): Integer;
     Public
         Function GetTaskMoney(Complexity: TComplexity): Integer;
         Function GetTaskXP(Complexity: TComplexity): Integer;
@@ -52,8 +91,12 @@ Type
 
         Function GetHpPersent(): TPercent;
         Function GetXpPersent(): TPercent;
+        Function GetBustsCost(): TBustsCost;
 
         Procedure ApplyNewTask(XP_Val, HP_Val, Coins_Val: Integer);
+        Procedure BuyBust(TypeOfBust: TBusts; CountLabel: TLabel);
+
+        Function IsCanBuy(ItemCost: Integer): Boolean;
 
         Property CurentLvl: Integer Read FCurentLvl;
         Property Coins: Integer Read FCoins;
@@ -61,10 +104,19 @@ Type
         Property XP: Integer Read FXP;
         Property MaxHP: Integer Read FNextLvlHP;
         Property MaxXP: Integer Read FNextLvlXP;
+        Property BustBought: TBustBought Read FBustBought;
         Constructor Create;
     End;
 
 Implementation
+
+Uses
+    TasksListScreenUnit;
+
+Function TUser.IsCanBuy(ItemCost: Integer): Boolean;
+Begin
+    IsCanBuy := FCoins >= ItemCost;
+End;
 
 Constructor TUser.Create;
 Begin
@@ -74,6 +126,44 @@ Begin
     FNextLvlHP := StartHP;
     FCoins := 0;
     FCurentLvl := 1;
+    FBustsBuyCount := 0;
+End;
+
+Procedure TUser.BuyBust(TypeOfBust: TBusts; CountLabel: TLabel);
+Var
+    LeftBorder: Integer;
+Begin
+    Inc(FBustBought[TypeOfBust]);
+    FCoins := FCoins - FBustsCost[TypeOfBust];
+    Inc(FBustsBuyCount);
+    LeftBorder := CountLabel.Left + CountLabel.Width;
+    CountLabel.Caption := 'x' + IntToStr(FBustBought[TypeOfBust]);
+    CountLabel.Left := LeftBorder - CountLabel.Width;
+End;
+
+Function TUser.FindCost(StartVal: Integer): Integer;
+Var
+    I: Integer;
+    ResVal: Double;
+Begin
+    ResVal := StartVal;
+    For I := 1 To FBustsBuyCount Do
+        ResVal := ResVal * CostsIncrease;
+    FindCost := Trunc(ResVal);
+End;
+
+Function TUser.GetBustsCost: TUser.TBustsCost;
+Begin
+    FBustsCost[TBusts.HP] := FindCost(HPCost);
+    FBustsCost[TBusts.XP] := FindCost(XPCost);
+    FBustsCost[TBusts.HPBust] := FindCost(HPBustCost);
+    FBustsCost[TBusts.XPBust] := FindCost(XPBustCost);
+    FBustsCost[TBusts.CoinsBust] := FindCost(CoinsBustCost);
+    FBustsCost[TBusts.Tothem] := FindCost(TothemBustCost);
+    FBustsCost[TBusts.FreeTask] := FindCost(FreeTaskBust);
+    FBustsCost[TBusts.SecretBox] := FindCost(SecretBox);
+
+    GetBustsCost := FBustsCost;
 End;
 
 Function TUser.CalculateAwards(Complexity: TComplexity; EasyVal, MediumVal, HardVal: Integer): Integer;
